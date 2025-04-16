d7f359b5d99a8cb957e087e7f1b1daec1d752cc2
camden.warburton <camden.warburton@ucalgary.ca>
2025-04-16 12:31:06 -0600

    Added a back button

---
+++ src/components/BackButton.js
@@ -0,0 +1,10 @@
+import { Box, Button } from "@mui/material";
+const BackButton = ({ onClick }) => {
+
+return <Box >
+    hello
+    <Button onClick= {onClick}>red</Button>
+</Box>
+
+}
+export default BackButton;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,18 +2,29 @@ import { Box } from "@mui/material"
 import Grid from "@mui/material/Grid2";
 
 import LoginCard from "../components/LoginCard";
+import BackButton from "../components/BackButton";
 
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+
+    // handle this later
     const loginPress = () => {
       console.log("login is pressed");
     }
 
+    // for the back button
+    const navigate = useNavigate();
+      const handleBack = () => {
+         navigate("/");
+      }
+
  return <>
  <Box>
+    <BackButton onClick={handleBack}/>
     <Grid 
     container 
     alignContent={"center"} 
