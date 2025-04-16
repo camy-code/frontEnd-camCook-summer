5b0c1f443d6761dc1061774a70117f54e7a3b66c
camden.warburton <camden.warburton@ucalgary.ca>
2025-04-16 12:39:54 -0600

    made a crisp back button

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -1,9 +1,23 @@
 import { Box, Button } from "@mui/material";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ColorPick from "../tools/ColorPick";
+import ButtonDesign from "../tools/ButtonDesign";
 const BackButton = ({ onClick }) => {
 
-return <Box >
-    hello
-    <Button onClick= {onClick}>red</Button>
+return <Box sx={[ButtonDesign.getButtonStyle,{
+    width:"15px",
+    height:"15px",
+    backgroundColor: ColorPick.getPrimary(),
+    color:"white", 
+    borderRadius:"50px",
+    alignItems:"center",
+    justifyContent:"center",
+    display:"flex",
+    padding:"25px",
+    
+    
+}]}>
+    <ArrowBackIcon sx={{transform:"scale(1.5)"}}/>
 </Box>
 
 }
