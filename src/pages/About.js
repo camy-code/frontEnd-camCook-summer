9a3094669db933ca7f553a662a3eb169b9e66329
camden.warburton <camden.warburton@ucalgary.ca>
2025-04-16 13:10:39 -0600

    Added a basic about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,25 @@
+import  Grid  from "@mui/material/Grid2"
+import { Box } from "@mui/material"
+
+import { useNavigate } from "react-router-dom";
+
+
+import BackButton from "../components/BackButton";
 const About = () => {
-    return <h1>Hello from about</h1>
+    const navigate = useNavigate();
+    const handleBack = () => {
+        navigate("/");
+    }
+
+    return <>
+     <Box sx={{padding:"25px"}}>
+    <BackButton onClick={handleBack}/>
+    </Box>
+    <Grid container direction="row" spacing={2} justifyContent={"center"} alignItems="center" sx={{height:"80vh", marginTop:-10}}>
+    <h1>About</h1>
+    <h1>About</h1>
+    </Grid>
+    </>
 }
 
 export default About
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,7 @@
 const Dashboard = () => {
+    const supperUser = true; // Need this for when an admin needs to do admin things
+
+    
     return <h1>This is my dashboard</h1>
 }
 
